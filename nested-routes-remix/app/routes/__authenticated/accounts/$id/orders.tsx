b3b4f4d10bcb690adb9fdc0f61f44244bf751ffc
Naresh Bhatia <naresh.archfirst@gmail.com>
2021-12-03 21:39:50 -0500

    Implemented Orders tab

--- nested-routes-remix/app/routes/__authenticated/accounts/$id/orders.tsx
+++ nested-routes-remix/app/routes/__authenticated/accounts/$id/orders.tsx
@@ -1,3 +1,48 @@
+import { json, useLoaderData } from 'remix';
+import type { LoaderFunction } from 'remix';
+import { Order } from '~/models';
+import { API_URL } from '~/utils';
+
+type OrdersViewData = {
+  orders: Array<Order>;
+};
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const { id } = params;
+  const resOrders = await fetch(`${API_URL}/orders?accountId=${id}`);
+  const orders = await resOrders.json();
+
+  const data: OrdersViewData = {
+    orders,
+  };
+
+  return json(data);
+};
+
 export default function OrdersView() {
-  return <div>Orders</div>;
+  const data = useLoaderData();
+  const orders = data.orders as Array<Order>;
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th className="text-left">Date</th>
+          <th className="text-left">Side</th>
+          <th className="text-left">Symbol</th>
+          <th className="text-left">Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {orders.map((order) => (
+          <tr key={order.id}>
+            <td>{order.createdAt}</td>
+            <td>{order.side}</td>
+            <td>{order.security.id}</td>
+            <td>{order.status}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
