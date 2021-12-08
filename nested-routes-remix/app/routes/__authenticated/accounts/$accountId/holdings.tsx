e32dbb0d412a026bf1645b77d7f937ef2ecea5c1
Naresh Bhatia <naresh.archfirst@gmail.com>
2021-12-08 00:24:46 -0500

    Fixed styles for HoldingsView

--- nested-routes-remix/app/routes/__authenticated/accounts/$accountId/holdings.tsx
+++ nested-routes-remix/app/routes/__authenticated/accounts/$accountId/holdings.tsx
@@ -1,5 +1,6 @@
 import { json, useLoaderData } from 'remix';
 import type { LoaderFunction } from 'remix';
+import { VerticalContainer } from '~/components';
 import { Holding } from '~/models';
 import { API_URL } from '~/utils';
 
@@ -24,27 +25,38 @@ export default function HoldingsView() {
   const holdings = data.holdings as Array<Holding>;
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th className="text-left">Symbol</th>
-          <th className="text-left">Name</th>
-          <th className="text-right">Qty</th>
-          <th className="text-right">Price</th>
-          <th className="text-right">Value</th>
-        </tr>
-      </thead>
-      <tbody>
-        {holdings.map((holding) => (
-          <tr key={holding.id}>
-            <td>{holding.security.id}</td>
-            <td>{holding.security.name}</td>
-            <td className="text-right">{holding.quantity}</td>
-            <td className="text-right">{holding.security.price}</td>
-            <td className="text-right">{holding.value}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <VerticalContainer className="paper border-paper p-4">
+      <h2>
+        {holdings && holdings.length > 0
+          ? 'Holdings'
+          : 'There are no holdings in this account'}
+      </h2>
+      {holdings && holdings.length > 0 && (
+        <VerticalContainer>
+          <table className="mt-4">
+            <thead>
+              <tr>
+                <th>Symbol</th>
+                <th>Name</th>
+                <th className="text-right">Qty</th>
+                <th className="text-right">Price</th>
+                <th className="text-right">Value</th>
+              </tr>
+            </thead>
+            <tbody>
+              {holdings.map((holding) => (
+                <tr key={holding.id}>
+                  <td>{holding.security.id}</td>
+                  <td>{holding.security.name}</td>
+                  <td className="text-right">{holding.quantity}</td>
+                  <td className="text-right">{holding.security.price}</td>
+                  <td className="text-right">{holding.value}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </VerticalContainer>
+      )}
+    </VerticalContainer>
   );
 }
