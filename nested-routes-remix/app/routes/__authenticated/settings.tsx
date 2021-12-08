e719b094c17fab5799f25e61aa7f1ac1bb75f852
Naresh Bhatia <naresh.archfirst@gmail.com>
2021-12-07 23:54:47 -0500

    Fixed styles for SettingsView

--- nested-routes-remix/app/routes/__authenticated/settings.tsx
+++ nested-routes-remix/app/routes/__authenticated/settings.tsx
@@ -4,8 +4,10 @@ import { VerticalContainer } from '~/components';
 export default function SettingsView() {
   return (
     <VerticalContainer className="p-4">
-      <h1>Settings</h1>
-      <Link to="/accounts">Back to Accounts</Link>
+      <h2>Settings</h2>
+      <Link className="link mt-4" to="/accounts">
+        Back to Accounts
+      </Link>
     </VerticalContainer>
   );
 }
