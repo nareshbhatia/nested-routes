f65989484fb11811f4c1f2c0a29d300fe01e98ef
Naresh Bhatia <naresh.archfirst@gmail.com>
2021-12-08 00:20:19 -0500

    Fixed styles for AccountHeader

--- nested-routes-remix/app/components/AccountHeader/AccountHeader.tsx
+++ nested-routes-remix/app/components/AccountHeader/AccountHeader.tsx
@@ -1,21 +1,33 @@
 import { NavLink } from 'remix';
 
 export const AccountHeader = () => {
-  const linkStyle = 'text-neutral-600 text-sm font-semibold mr-5';
+  const linkStyle = 'hover:text-neutral-900 text-sm font-semibold mr-5';
+
+  const links = [
+    { to: 'holdings', label: 'Holdings' },
+    { to: 'orders', label: 'Orders' },
+  ];
 
   return (
     <nav className="flex items-center h-14 px-4 bg-neutral-50">
       <ul className="flex">
-        <li>
-          <NavLink className={linkStyle} to="holdings" end>
-            Holdings
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className={linkStyle} to="orders" end>
-            Orders
-          </NavLink>
-        </li>
+        {links.map((link) => {
+          return (
+            <li key={link.to}>
+              <NavLink
+                to={link.to}
+                className={({ isActive }) =>
+                  isActive
+                    ? `${linkStyle} text-neutral-900`
+                    : `${linkStyle} text-neutral-400`
+                }
+                end
+              >
+                {link.label}
+              </NavLink>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
